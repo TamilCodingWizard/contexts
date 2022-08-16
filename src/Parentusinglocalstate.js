0e760143c3e4a950b5623af47d0e19c2b7340aa5
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-08-16 22:57:12 +0100

    Context Tutorial

---
+++ src/Parentusinglocalstate.js
@@ -0,0 +1,59 @@
+
+
+import React, { useState } from 'react'
+
+export default function Parentusinglocalstate() {
+  const [user] = useState('React')
+  return (
+    <>
+    <h1>Parentusinglocalstate</h1>
+     <Component1 user={user}/>
+    </>
+  )
+}
+
+function Component1({user}) {
+    return (
+        <>
+           <h3>Component 1</h3>
+           <Component2 user={user}/>
+        </>
+    )
+}
+
+function Component2({user}) {
+    return (
+        <>
+           <h3>Component 2</h3>
+           <Component3 user={user}/>
+        </>
+    )
+}
+
+function Component3({user}) {
+    return (
+        <>
+           <h3>Component 3</h3>
+           <Component4 user={user}/>
+
+        </>
+    )
+}
+
+function Component4({user}) {
+    return (
+        <>
+           <h3>Component 4</h3>
+           <Component5 user={user}/>
+        </>
+    )
+}
+
+function Component5({user}) {
+    return (
+        <>
+           <h3>Component 5</h3>
+           <h1>{`user is ${user}`}</h1>
+        </>
+    )
+}
